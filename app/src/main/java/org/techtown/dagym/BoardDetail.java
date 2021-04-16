106de088ae1b02aa1fd1d09c31dedf7379d0994a
JiALee <ljahpoao@naver.com>
2021-04-16 09:36:11 +0900

    회원가입 뷰 수정

--- app/src/main/java/org/techtown/dagym/BoardDetail.java
+++ app/src/main/java/org/techtown/dagym/BoardDetail.java
@@ -3,6 +3,7 @@ package org.techtown.dagym;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ import org.techtown.dagym.databinding.ActivityWriteBoardBinding;
 import org.techtown.dagym.databinding.BoardDetailBinding;
 import org.techtown.dagym.entity.Board;
 import org.techtown.dagym.entity.dto.BoardListResponseDto;
+import org.techtown.dagym.session.SharedPreference;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -30,24 +32,6 @@ public class BoardDetail extends AppCompatActivity {
         setContentView(b.getRoot());
 
         long id = getIntent().getExtras().getLong("id");
-        dataService.select.idSelect(id).enqueue(new Callback<Board>() {
-            @Override
-            public void onResponse(Call<Board> call, Response<Board> response) {
-                Board body = response.body();
-                String strDate = body.getModDate();
-                LocalDateTime localDateTime = LocalDateTime.parse(strDate, DateTimeFormatter.ISO_DATE_TIME);
-                String modDate = localDateTime.format(DateTimeFormatter.ofPattern("yy/MM/dd hh:mm"));
-
-                b.textViewTitle.setText(body.getTitle());
-                b.boardContent.setText(body.getContent());
-                b.textViewDay.setText(modDate);
-            }
-
-            @Override
-            public void onFailure(Call<Board> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
 
         //글 수정 버튼
         b.modifyWrite.setOnClickListener(v -> {
@@ -72,7 +56,36 @@ public class BoardDetail extends AppCompatActivity {
             finish();
         });
 
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        long id = getIntent().getExtras().getLong("id");
+        dataService.select.idSelect(id).enqueue(new Callback<Board>() {
+            @Override
+            public void onResponse(Call<Board> call, Response<Board> response) {
+                Board body = response.body();
+                String strDate = body.getModDate();
+                LocalDateTime localDateTime = LocalDateTime.parse(strDate, DateTimeFormatter.ISO_DATE_TIME);
+                String modDate = localDateTime.format(DateTimeFormatter.ofPattern("yy/MM/dd hh:mm"));
+
+                b.textViewTitle.setText(body.getTitle());
+                b.boardContent.setText(body.getContent());
+                b.textViewDay.setText(modDate);
+
+                String user_id = SharedPreference.getAttribute(getApplicationContext(), "user_id");
+                if(!user_id.equals(body.getUser_id())) {
+                    b.deleteWrite.setVisibility(View.GONE);
+                    b.modifyWrite.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Board> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
