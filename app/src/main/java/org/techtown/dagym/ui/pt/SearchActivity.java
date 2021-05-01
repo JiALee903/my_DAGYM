e7eed09b62452e0271972bdf82a614819c8c3a23
JiALee <ljahpoao@naver.com>
2021-05-01 21:59:55 +0900

    PT 신청 레이아웃

---
+++ app/src/main/java/org/techtown/dagym/ui/pt/SearchActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.dagym.ui.pt;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import org.techtown.dagym.R;
+import org.techtown.dagym.databinding.ActivityPersonaltBinding;
+import org.techtown.dagym.databinding.ActivitySearchfBinding;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private ActivitySearchfBinding b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        b = b.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+
+
+    }
+}
