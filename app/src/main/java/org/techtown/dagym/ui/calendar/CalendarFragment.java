b6cb34fa9931dfa4483805893482a0e3efa02cbe
JiALee <ljahpoao@naver.com>
2021-05-11 15:43:17 +0900

    캘린더 등록 삭제 레이아웃

--- app/src/main/java/org/techtown/dagym/ui/calendar/CalendarFragment.java
+++ app/src/main/java/org/techtown/dagym/ui/calendar/CalendarFragment.java
@@ -19,20 +19,26 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import org.techtown.dagym.R;
 
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 
 public class CalendarFragment extends Fragment {
+
+    private String day_if = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,6 +58,22 @@ public class CalendarFragment extends Fragment {
                 oneDayDecorator,
                 new EventDecorator(Color.RED, Collections.singleton(CalendarDay.today()))
         );
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                String mDate = date.getYear() + "/" + (date.getMonth() + 1) + "/" + date.getDay();
+                Log.i("TAG", "onDateSelected: date = " + mDate);
+
+                if(mDate.equals(day_if)) {
+                    Log.i("TAG", "성공!");
+                    Intent intent = new Intent(getContext(), CalendarRecord.class);
+                    startActivity(intent);
+                } else {
+                    day_if = mDate;
+                }
+            }
+        });
 
         return view;
     }
